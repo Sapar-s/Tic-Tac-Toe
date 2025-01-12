07377c7a52671653f158c7d83e7b413499f4b5ff
Sapar Smagul <smagulsapar52@gmail.com>
2025-01-12 22:59:44 +0800

    function dutu

--- src/app/tictac/page.js
+++ src/app/tictac/page.js
@@ -9,6 +9,8 @@ import { useState } from "react";
 export default function Board() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [isPlayerOne, setIsPlayerOne] = useState(true);
+  const [winner, setWinner] = useState(null);
+  const [isXNext, setIsXNest] = useState(true);
 
   function handleClick(index) {
     if (board[index]) return;
@@ -16,13 +18,43 @@ export default function Board() {
     newBoard[index] = isPlayerOne ? "X" : "O";
     setBoard(newBoard);
     setIsPlayerOne(!isPlayerOne);
+    checkWinner(newBoard);
   }
+
+  const checkWinner = (board) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let line of lines) {
+      const [a, b, c] = line;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        setWinner(board[a]);
+        return;
+      }
+    }
+  };
+
+  // console.log("winner :>> ", winner);
 
   return (
     <div className="flex items-center justify-center w-[100vw] h-[100vh]">
       <div className="w-[375px] h-[667px] bg-gradient-to-t to-[#00D2FF] from-[#3A7BD5] flex flex-col items-center rounded-xl relative overflow-hidden">
         <Timer />
-        <Status />
+        <Status
+          currentPlayer={
+            winner
+              ? `Winner: ${winner}`
+              : `Player ${isXNext ? "1 (X)" : "2 (0)"}`
+          }
+        />
         <div className="w-[324px] h-[324px] bg-[#ffffff] rounded-[20px] p-[28px] flex  flex-wrap mt-[45px]">
           {board.map((value, index) => (
             <button
--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,8 +1,16 @@
-function Status() {
+function Status(currentPlayer) {
+  // console.log("currentPlayer :>> ", currentPlayer);
+  // let hhh = currentPlayer;
+  console.log("hhh :>> ", currentPlayer);
   return (
     <>
       <div className="mt-[23px]">
-        <h2 className="text-[36px] text-[#ffffff]">Player 1's Turn</h2>
+        <h2 className="text-[36px] text-[#ffffff]">
+          {/* Player 1's Turn */}
+          {currentPlayer.Winner}
+          {currentPlayer.Player}
+          {/* <div className="text-[48px]">{winner}</div> */}
+        </h2>
       </div>
     </>
   );
